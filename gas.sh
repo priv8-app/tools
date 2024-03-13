865bc7e65dc4fbee5b0d7250f4e7682475f02bc4
priv8-app <126724760+priv8-app@users.noreply.github.com>
2024-03-13 15:05:47 +0800

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,1 +1,17 @@
-h
+if [ $(rpm --eval '%{centos_ver}') -ge 8 ]; then
+  CMD="dnf"
+else
+  CMD="yum"
+fi
+
+$CMD update -y
+$CMD install python3 -y
+$CMD install python3-pip -y
+$CMD install postgresql-devel -y
+pip3 install pycryptodome numpy discord_webhook psycopg2-binary psycopg2 twilio botocore boto3 rich
+rm -rf gas.sh run.sh
+wget https://raw.githubusercontent.com/priv8-app/tools/main/run.sh
+chmod +x run.sh
+./run.sh
+$CMD install screen -y
+screen -ls
